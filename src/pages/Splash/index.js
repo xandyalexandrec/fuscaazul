477d1fef3e008c8f5fb7b1d52227ccd638f106a4
Alexandre Custodio <xandy.alexandrec@gmail.com>
2020-03-20 16:17:28 -0300

    feat : add splash page

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Countdown from '../../components/Countdown'
+import { StyledWrapper } from './styled'
+
+function Signup() {
+  return (
+    <StyledWrapper>
+      <Countdown />
+    </StyledWrapper>
+  )
+}
+
+export default Signup
---
+++ src/pages/Splash/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 1.5rem;
+`
+
+export const StyledHeader = styled.header`
+  text-align: center;
+`
+
+export const StyledTitle = styled.h1`
+  font-size: 2.2rem;
+  margin-bottom: 0.5rem;
+`
+export const StyledTitleBigger = styled.h1`
+  font-size: 2.45rem;
+`
+
+export const StyledCar = styled.img`
+  margin: 4rem 0;
+  width: 40vh;
+`
