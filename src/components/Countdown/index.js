71e4669857fd2b6e6eefef2510c5d95f483ebbad
Alexandre Custodio <xandy.alexandrec@gmail.com>
2020-03-21 00:33:45 -0300

    refactor: receive handlefinish as param

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -2,10 +2,7 @@ import React from 'react'
 import { StyledWrapper, StyledCard, StyledNumber } from './styled'
 import useCountdown from '../../hooks/useCountdown/'
 
-function Countdown() {
-  const handleFinish = () => {
-    console.log ('acabou')
-  }
+function Countdown({ handleFinish = () => {} }) {
   const [counter] = useCountdown({ handleFinish })
   return (
     <StyledWrapper>
