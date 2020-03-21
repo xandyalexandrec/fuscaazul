4c83ff3b92a5b698cfe67db5134c8eec61730eb5
Alexandre Custodio <xandy.alexandrec@gmail.com>
2020-03-21 00:34:47 -0300

    feat: add state into signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import Context from '../../utils/context'
+import { SPLASH } from '../../router'
 import Input from '../../components/Input'
+import Error from '../../components/Error'
 import Button from '../../components/Button'
 import Car from '../../assets/images/car.png';
 import { StyledWrapper, StyledHeader, StyledTitle, StyledTitleBigger, StyledCar } from './styled'
 
 function Signup() {
+  const { player, setPlayer, setCurrentRoute } = useContext(Context)
+  const [name, setName] = useState(player.name)
+  const [error, setError] = useState(null)
+
+  const handleSubmit = () => {
+    if (name && name.length > 2) {
+      setPlayer({
+        ...player,
+        name: name
+      })
+      setCurrentRoute(SPLASH)
+    } else {
+      setError('Must have at least two letters')
+    }
+  }
+
   return (
     <StyledWrapper>
       <StyledHeader>
@@ -12,8 +31,14 @@ function Signup() {
         <StyledTitle>FUSCA AZUL</StyledTitle>
         <StyledTitleBigger>CHALLENGE</StyledTitleBigger>
       </StyledHeader>
-      <Input label="Your name" />
-      <Button>Start game</Button>
+      <Input
+        label="Your name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        focus
+      />
+      {error && <Error>{error}</Error>}
+      <Button onClick={handleSubmit}>Start game</Button>
     </StyledWrapper>
   )
 }
