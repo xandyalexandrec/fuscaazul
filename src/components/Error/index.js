71024a00055f8b096290c474e7615a4f64d2aa64
Alexandre Custodio <xandy.alexandrec@gmail.com>
2020-03-21 00:34:30 -0300

    feat: error text component

---
+++ src/components/Error/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { DANGER_COLOR } from '../../utils/colors'
+
+export default styled.p`
+  color: ${DANGER_COLOR};
+  margin: 0.3rem 0 1rem;
+`
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,5 @@
 export const PRIMARY_COLOR = '#5FD7DB'
-export const SUCCESS_COLOR = '#92cc41'
+export const SUCCESS_COLOR = '#92CC41'
+export const DANGER_COLOR = '#FF1100'
 export const DARK_COLOR = "#000000"
 
