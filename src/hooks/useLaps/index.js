5392ecb67cd130c63322ae7520c294788cf2eee0
Alexandre Custodio <xandy.alexandrec@gmail.com>
2020-03-21 18:37:10 -0300

    refactor: more realist speed

--- src/hooks/useLaps/index.js
+++ src/hooks/useLaps/index.js
@@ -4,8 +4,22 @@ import useTurbo from '../useTurbo'
 const LAP_DISTANCE = 500000
 const INITIAL_LAP = 0
 const INITIAL_SPEED = 60
-const SPEED_INCREASE = 1
-const SPEED_INCRESE_WITH_TURBO = 5
+const INCREASE_SPEED_TURBO = 7
+const INCREASE_MIN_SPEED = -2
+const INCREASE_MAX_SPEED = 4
+
+const calculateSpeed = ({ speed, turbo }) => {
+  // If turbo is active increase speed by 7
+  if(turbo) {
+    return speed + INCREASE_SPEED_TURBO
+  }
+  // Without turbo it increase speed between -2 and 4
+  return speed + getRandomNumber(INCREASE_MIN_SPEED, INCREASE_MAX_SPEED)
+}
+
+const getRandomNumber = (min, max) => {
+  return Math.floor((Math.random() * (max - min) + min))
+}
 
 const useLaps = () => {
   const [lap, setLap] = useState(INITIAL_LAP)
@@ -16,8 +30,7 @@ const useLaps = () => {
 
   useEffect(() => {
     if (!paused) {
-      const increase = turbo ? SPEED_INCRESE_WITH_TURBO : SPEED_INCREASE
-      setTimeout(() => setSpeed(speed + increase), 1000)
+      setTimeout(() => setSpeed(calculateSpeed({ speed, turbo })), 1000)
       setTimeout(() => setLap(lap + 1), LAP_DISTANCE / speed)
       setTimeout(() => setDuration(duration + 1), 1000)
     }
