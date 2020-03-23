2e3355c2a012c308dbdaeaa83113656e7ab6cfa4
Alexandre Custodio <xandy.alexandrec@gmail.com>
2020-03-23 00:29:08 -0300

    refactor: game controls to not use external lib

--- src/pages/Game/useControls.js
+++ src/pages/Game/useControls.js
@@ -1,16 +1,32 @@
-import { useHotkeys } from 'react-hotkeys-hook'
 import useGestures from 'use-gestures'
 
-const useControls = ({ positionActions, setPaused, startTurbo, scene }) => {
-  useHotkeys('t', () => startTurbo())
-  useHotkeys('esc', () => setPaused(true))
-  useHotkeys('space', () => setPaused(false))
+const A = 65
+const S = 83
+const D = 68
+const LEFT = 37
+const RIGHT = 39
+const T = 84
+const ESC = 27
+const SPACE = 32
 
-  useHotkeys('a', () => positionActions.moveLeftPosition())
-  useHotkeys('s', () => positionActions.moveMiddlePosition())
-  useHotkeys('d', () => positionActions.moveRightPosition())
-  useHotkeys('left', () => positionActions.moveLeft())
-  useHotkeys('right', () =>positionActions.moveRight())
+const useControls = ({ positionActions, setPaused, startTurbo, scene }) => {
+  const keyboardActions = {
+    [A]: () => positionActions.moveLeftPosition(),
+    [S]: () => positionActions.moveMiddlePosition(),
+    [D]: () => positionActions.moveRightPosition(),
+    [LEFT]: () => positionActions.moveLeft(),
+    [RIGHT]: () => positionActions.moveRight(),
+    [T]: () => startTurbo(),
+    [ESC]: () => setPaused(true),
+    [SPACE]: () => setPaused(false),
+  }
+
+  document.addEventListener('keydown', ({ keyCode }) => {
+    const action = keyboardActions[keyCode];
+    if (typeof action === 'function') {
+      action()
+    }
+  })
 
   useGestures(scene, {
     onSwipeLeft: () => positionActions.moveLeft(),
